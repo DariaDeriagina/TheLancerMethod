85e9d2ad69bc4a0311eb60f059cdf2d69ca125d0
Daria Deriagina <w0862447@myscc.ca>
2025-06-25 13:24:23 -0400

    Buttons added to Links

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-// script.js
+document.addEventListener("DOMContentLoaded", () => {
+	const burger = document.getElementById("burgerMenu");
+	const menuContainer = document.getElementById("menuContainer");
 
-// Функция для показа элементов с классом .animate
-function animateOnScroll() {
-	const animatedElements = document.querySelectorAll(".animate");
-
-	const observer = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					entry.target.classList.add("show");
-					observer.unobserve(entry.target);
-				}
-			});
-		},
-		{
-			threshold: 0.2,
-		}
-	);
-
-	animatedElements.forEach((el) => {
-		observer.observe(el);
+	burger.addEventListener("click", () => {
+		menuContainer.classList.toggle("active");
 	});
-}
-
-// Запуск после загрузки
-document.addEventListener("DOMContentLoaded", animateOnScroll);
+});
