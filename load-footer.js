225f15238d405eebb7dc611551c8131a6e0c14f0
Daria Deriagina <w0862447@myscc.ca>
2025-07-29 21:58:11 -0400

    Footer html and load-footer.js added

---
+++ load-footer.js
@@ -0,0 +1,9 @@
+// load-footer.js
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("footer.html")
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById("footer-placeholder").innerHTML = data;
+		})
+		.catch((error) => console.error("Error loading footer:", error));
+});
